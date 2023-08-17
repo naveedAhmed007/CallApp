946b3377b5df21624dc42a5d8baf62899aaa7109
naveedAhmed007 <naveed.ahmed007@ymail.com>
2023-08-17 12:44:18 +0500

    Update AdminBottomNavigation.java

--- app/src/main/java/com/itoasis/callingapp/Activities/AdminBottomNavigation.java
+++ app/src/main/java/com/itoasis/callingapp/Activities/AdminBottomNavigation.java
@@ -1,11 +1,22 @@
 package com.itoasis.callingapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import androidx.core.view.MenuCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.itoasis.callingapp.Fragments.Home;
@@ -35,11 +46,11 @@ public class AdminBottomNavigation extends AppCompatActivity {
                     setCurrentFragment(new Home());
                     break;
                 case R.id.summary:
-                    setCurrentFragment(new Home());
+
                     break;
-//                case R.id.history:
-//                    setCurrentFragment(new Home());
-//                    break;
+                case R.id.history:
+                    showPopupMenu(bottomNavigationView);
+                    break;
 //                case R.id.logout:
 //                    setCurrentFragment(new Home());
 //                    break;
@@ -54,4 +65,32 @@ public class AdminBottomNavigation extends AppCompatActivity {
         fragmentTransaction.replace(R.id.flFragment, fragment);
         fragmentTransaction.commit();
     }
+    private void showPopupMenu(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.inflate(R.menu.admin_bottom); // Replace with your actual menu resource
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.prices:
+                        Toast.makeText(AdminBottomNavigation.this, "aaaaaaaaaaaaaaaaaaaaaaaaaaaa", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+        // Adjust popup menu gravity to show at the bottom end
+        popupMenu.setGravity(Gravity.END);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            popupMenu.setForceShowIcon(true);
+        }
+       
+        popupMenu.show();
+    }
+
+
+
+
+
+
     }
