ffe1a4faf645ea1fbabbac3c7c2fb45fabafd2aa
naveedAhmed007 <naveed.ahmed007@ymail.com>
2023-08-18 18:19:08 +0500

    Summary

--- app/src/main/java/com/itoasis/callingapp/Fragments/Summary.java
+++ app/src/main/java/com/itoasis/callingapp/Fragments/Summary.java
@@ -1,25 +1,34 @@
 package com.itoasis.callingapp.Fragments;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.PopupMenu;
 
 import com.itoasis.callingapp.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public class Summary extends Fragment {
+int mDay,mMonth,mYear;
 
 
 
 
 
 
-
-
+Button monthPicker;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +40,30 @@ public class Summary extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_summary, container, false);
-
+monthPicker=v.findViewById(R.id.button);
+monthPicker.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        showMonthPickerMenu();
+    }
+});
         return v;
+    }
+
+
+    private void showMonthPickerMenu() {
+        PopupMenu popupMenu = new PopupMenu(requireContext(), monthPicker);
+        popupMenu.getMenuInflater().inflate(R.menu.month_picker_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String selectedMonth = item.getTitle().toString();
+                // Do something with the selected month
+                return true;
+            }
+        });
+
+        popupMenu.show();
     }
 }
